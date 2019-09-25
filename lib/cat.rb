4d1f5ca899db6b7dcbdd6b426127ac6abcf81e19
Micah Levine <micah.levine@outlook.com>
2019-09-25 11:46:50 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,13 +1,19 @@
 class Cat
 
-  attr_accessor :mood
+  attr_accessor :mood, :owner
   attr_accessor :name
 
-  def initialize(name)
+@@all = []
+
+  def initialize(name, owner)
     @name = name
     @mood = "nervous"
+    @owner = owner
+    @@all << self
   end
   
-
+def self.all
+  @@all
+end
 
 end
